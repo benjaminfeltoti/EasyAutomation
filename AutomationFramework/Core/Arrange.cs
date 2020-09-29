18ae027becd38397de53e1c0c178c90e347c7a77
benjamin.feltoti@gmail.com <benjamin.feltoti@gmail.com>
2020-09-30 01:01:09 +0200

    Fix Arrange class, make it safe from process business.

--- AutomationFramework/Core/Arrange.cs
+++ AutomationFramework/Core/Arrange.cs
@@ -1,5 +1,6 @@
 ﻿using EasyAutomation.AutomationFramework.Logging;
 using System;
+using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Automation;
@@ -24,9 +25,48 @@ namespace EasyAutomation.AutomationFramework.Core
 
     internal static class Arrange<T>
     {
+        /* var task = Task.Factory.StartNew<T, ElementInformation>(() =>
+            {
+                string name = "???";
+                string automationId = "???";
+                string localizedControlType = "???";
+
+                Process[] p = Process.GetProcessesByName("ExampleApplication");
+
+                while (p.Length > 0 && !taskCancellationToken.IsCancellationRequested)
+                {
+                    if (p[0].Responding)
+                    {
+                        try
+                        {
+                            var resultProperty = (T)automationElement.GetCurrentPropertyValue(automationProperty);
+                            name = (string)automationElement.GetCurrentPropertyValue(AutomationElement.NameProperty, false);
+                            automationId = (string)automationElement.GetCurrentPropertyValue(AutomationElement.AutomationIdProperty, false);
+                            localizedControlType = (string)automationElement.GetCurrentPropertyValue(AutomationElement.LocalizedControlTypeProperty, false);
+
+                            return new Tuple<T, ElementInformation>(resultProperty, new ElementInformation(name, automationId, localizedControlType));
+                        }
+                        catch (Exception e)
+                        {
+                            string errorMessage = $"ERROR : Could not get property: { automationProperty.ProgrammaticName } on element:" +
+                                $" Name: {name} AutomationId: {automationId} ControlType: {localizedControlType} ; {e.Message}";
+
+                            Log.Write(errorMessage, TextType.FatalError);
+                            throw new Exception(errorMessage);
+                        }
+                    }
+
+                    Thread.Sleep(200);
+                }
+
+                return new Tuple<T, ElementInformation>(default(T), new ElementInformation(name, automationId, localizedControlType));
+*/
+
         internal static T GetProperty(AutomationElement automationElement, AutomationProperty automationProperty, uint timeLimit = 5000)
         {
-            var limit = TimeSpan.FromMilliseconds(timeLimit);
+            var limit = (int)timeLimit;
+
+            var taskCancellationToken = new CancellationTokenSource();
 
             var task = Task.Factory.StartNew(() =>
             {
@@ -34,26 +74,43 @@ namespace EasyAutomation.AutomationFramework.Core
                 string automationId = "???";
                 string localizedControlType = "???";
 
-                try
-                {                    
-                    var resultProperty = (T)automationElement.GetCurrentPropertyValue(automationProperty);
-                    name = (string)automationElement.GetCurrentPropertyValue(AutomationElement.NameProperty, false);
-                    automationId = (string)automationElement.GetCurrentPropertyValue(AutomationElement.AutomationIdProperty, false);
-                    localizedControlType = (string)automationElement.GetCurrentPropertyValue(AutomationElement.LocalizedControlTypeProperty, false);
+                Process[] p = Process.GetProcessesByName("ExampleWinformsApplication");
 
-                    return new Tuple<T, ElementInformation>(resultProperty, new ElementInformation(name, automationId, localizedControlType));
-                }
-                catch (Exception e)
+                while (p.Length > 0 && !taskCancellationToken.IsCancellationRequested)
                 {
-                    string errorMessage = $"ERROR : Could not get property: { automationProperty.ProgrammaticName } on element:" +
-                        $" Name: {name} AutomationId: {automationId} ControlType: {localizedControlType} ; {e.Message}";
+                    if (p[0].Responding)
+                    {
+                        try
+                        {
+                            var resultProperty = (T)automationElement.GetCurrentPropertyValue(automationProperty);
+                            name = (string)automationElement.GetCurrentPropertyValue(AutomationElement.NameProperty, false);
+                            automationId = (string)automationElement.GetCurrentPropertyValue(AutomationElement.AutomationIdProperty, false);
+                            localizedControlType = (string)automationElement.GetCurrentPropertyValue(AutomationElement.LocalizedControlTypeProperty, false);
 
-                    Log.Write(errorMessage, TextType.FatalError);
-                    throw new Exception(errorMessage);
+                            return new Tuple<T, ElementInformation>(resultProperty, new ElementInformation(name, automationId, localizedControlType));
+                        }
+                        catch (Exception e)
+                        {
+                            string errorMessage = $"ERROR : Could not get property: { automationProperty.ProgrammaticName } on element:" +
+                                $" Name: {name} AutomationId: {automationId} ControlType: {localizedControlType} ; {e.Message}";
+
+                            Log.Write(errorMessage, TextType.FatalError);
+                            throw new Exception(errorMessage);
+                        }
+                    }
+
+                    Thread.Sleep(200);
                 }
-            });
+
+                return new Tuple<T, ElementInformation>(default, new ElementInformation(name, automationId, localizedControlType));
+
+            }, taskCancellationToken.Token);
 
             bool taskIsSuccessfull = task.Wait(limit);
+
+            taskCancellationToken.Cancel();
+            taskCancellationToken.Dispose();
+
             var result = task.Result;
 
             if (task.IsCompleted)
